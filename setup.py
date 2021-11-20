991f03ef0966953711280d0814dc39ae2541a3a3
Satwik-Bhagwat <69520386+Satwik-Bhagwat@users.noreply.github.com>
2021-11-20 23:30:15 +0530

    Update setup.py
    creating datanodes

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 import os
 import json
 
-def init_DFS(config_file_path = ''):
-	if config_file_path == '':
-		config_file_path = 'default_config.json'
-
+def init_DFS(config_file_path = 'default_config.json'):
 	config_file = open(config_file_path)
 	config = json.load(config_file)
 	print(json.dumps(config, indent=4))
 
+	parent_path = '/home/pes1ug19cs438/Desktop/BD_assignments/Project/hadoop2021_bigdata-main/'
+	directory = 'DataNodes'
+	cur_path = os.path.join(parent_path, directory)
+	os.mkdir(cur_path)
+	
+	no_of_nodes = config["datanode_size"]
+	for i in range(no_of_nodes):
+		dirname = 'DN'+str(i+1)
+		path = os.path.join(cur_path, dirname)
+		os.mkdir(path)
 
 init_DFS('config_sample.json')
