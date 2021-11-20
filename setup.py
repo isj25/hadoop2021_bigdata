f137cc67cf9232390092d1f00927e2c6c184d189
Satwik-Bhagwat <69520386+Satwik-Bhagwat@users.noreply.github.com>
2021-11-21 00:33:40 +0530

    Update setup.py
    Splitting file to datanodes

--- setup.py
+++ setup.py
@@ -1,23 +1,42 @@
 import os
 import json
+from fsplit.filesplit import Filesplit
+
+fs = Filesplit()
 
 def init_DFS(config_file_path = 'default_config.json'):
 	config_file = open(config_file_path)
 	config = json.load(config_file)
-	print(json.dumps(config, indent=4))
 
 	USER = os.path.expanduser('~')
 	parent_path = f'{USER}/DATANODE/'
-	print(parent_path)
+	
 	os.mkdir(parent_path)
+	
 	directory = 'DataNodes'
 	cur_path = os.path.join(parent_path, directory)
 	os.mkdir(cur_path)
 	
 	no_of_nodes = config["datanode_size"]
 	for i in range(no_of_nodes):
-		dirname = 'DN'+str(i+1)
+		dirname = 'DN'+str(i)
 		path = os.path.join(cur_path, dirname)
 		os.mkdir(path)
+
+	dirname = 'temp'
+	path = os.path.join(cur_path, dirname)
+	os.mkdir(path)
+
+	block_size = config["block_size"]
+
+	temp_path = os.path.join(cur_path, 'temp/')
+	src_file = f'{USER}/sample.json'
+
+	fs.split(file=src_file, split_size=block_size, output_dir=temp_path)
+	  
+	allfiles = os.listdir(temp_path)
+	
+	for index,file in enumerate(allfiles):
+	    os.rename(temp_path + file, cur_path + '/DN' + str(index%no_of_nodes) + '/' + file)
 
 init_DFS('config_sample.json')
