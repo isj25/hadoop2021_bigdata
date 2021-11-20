9c8fc32ad9288d951cca9f4a46890f89db0b0835
Shreevathsa B K <75425624+ShreevathsaBK@users.noreply.github.com>
2021-11-20 19:55:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import argparse
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
+	parser = argparse.ArgumentParser(description='Command line interface for YAH')
 	parser.add_argument('command', choices=['put', 'cat', 'ls', 'rm', 'mkdir', 'rmdir'])
+	parser.add_argument('--arg1')
+	parser.add_argument('--arg2')
+	
 	args = parser.parse_args()
-	
 	command = args.command
-	
+
 	if command == 'put':
 		pass
 	elif command == 'cat':
