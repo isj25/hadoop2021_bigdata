9b6146b77d73884702a20ddea7fa8a046b5f4323
isj25 <ishwarj515@gmail.com>
2021-12-05 12:47:22 +0530

    added namenode log file to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 import json
+import datetime
 
 def init_DFS(config_file_path = 'default_config.json'):
 	config_file = open(config_file_path)
@@ -9,13 +10,23 @@ def init_DFS(config_file_path = 'default_config.json'):
 	namenode = os.path.expandvars(config['path_to_namenodes'])
 	DFS = os.path.expandvars(config['fs_path']) # FS PATH
 	
+	
 	os.mkdir(datanode)   	#creating datanode
 	os.mkdir(namenode)	    #creating namenode
 	os.makedirs(DFS)        #root
 	
+	name_node_logfile_path = os.path.expandvars(config['namenode_log_path'])
+	namenode_log_file = open(name_node_logfile_path,'a+')
+	namenode_log_file.write(str(datetime.datetime.now()) + ": created namenode log file\n")
+
 	mapping_file = open(namenode + 'mapping_file.json','w')
+	namenode_log_file.write(str(datetime.datetime.now()) + ": created namenode mapping file\n")
+
 	location_file = open(namenode + 'location_file.json','w')
+	namenode_log_file.write(str(datetime.datetime.now()) + ": created namenode location_file\n")
+
 	datanode_tracker = open(namenode + 'datanode_tracker.json', 'w')
+	namenode_log_file.write(str(datetime.datetime.now()) + ": created namenode log file\n")
 
 	datanode_data = {}
 	datanode_data['Next_datanode'] = 1
@@ -32,7 +43,7 @@ def init_DFS(config_file_path = 'default_config.json'):
 		dirname = 'DN' + str(i+1)
 		path = os.path.join(cur_path, dirname)
 		os.mkdir(path)
-		
+		namenode_log_file.write(str(datetime.datetime.now()) + ": created datanode " + dirname +"\n")
 		datanode_data[dirname] = blocks
 
 	datanode_tracker.write(json.dumps(datanode_data, indent=4))
@@ -46,5 +57,5 @@ def init_DFS(config_file_path = 'default_config.json'):
 	dfs_setup = open("dfs_setup_config.json",'w') 
 	dfs_setup.write(json.dumps(config,indent=4))
 	dfs_setup.close()
-
+	namenode_log_file.close()
 init_DFS('config_sample.json')
