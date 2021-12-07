9fe858c9b37368908fd8f2d3d2b511726db17502
Shreevathsa B K <75425624+ShreevathsaBK@users.noreply.github.com>
2021-12-07 22:13:40 +0530

    Update zookeeper.py

--- zookeeper.py
+++ zookeeper.py
@@ -11,7 +11,11 @@ config = json.load(config_file)
 namenode = os.path.expandvars(config['path_to_namenodes'])
 sec_namenode = os.path.expandvars(config['secondary_namenode_path'])
 sync = config['sync_period']
+
 while(True):
     time.sleep(sync)
     if(not os.path.isdir(namenode)):
         shutil.copytree(sec_namenode,namenode)
+        print("namenode retrived from backup")
+    else:
+        print("everything ok")
